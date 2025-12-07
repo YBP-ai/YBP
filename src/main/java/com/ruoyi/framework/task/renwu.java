90ee4a2124bda3930b25864935ab3fa57505d50a
YYY <553415319@qq.com>
2025-12-07 14:34:25 +0800

    renwu3

--- src/main/java/com/ruoyi/framework/task/renwu.java
+++ src/main/java/com/ruoyi/framework/task/renwu.java
@@ -8,7 +8,7 @@ public class renwu {
     public void ryParams(String params)
     {
         System.out.println("执行有参方法：" + params);
-        System.out.println("执行有参方法：" + params);
+        System.out.println("执行有参方法0：" + params);
         System.out.println("执行有参方法1：" + params);
         System.out.println("执行有参方法2：");
 
--- src/main/java/com/ruoyi/framework/task/renwu2.java
+++ src/main/java/com/ruoyi/framework/task/renwu2.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component("renwu2")
 public class renwu2 {
-    public void print()
+    public void print(String params)
     {
         System.out.println("执行没有参方法：" + params);
-
+        System.out.println("执行没有参方法2：" + params);
     }
 }
